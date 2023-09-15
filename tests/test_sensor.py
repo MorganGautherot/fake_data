d3c6e1e834c54195a2ef6a72155fa47ec3ba324c
Gautherot <mo.gautherot@laposte.net>
2023-09-15 18:29:05 +0200

    add parent directory to sys to run test

--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -1,6 +1,14 @@
 from sensor import VisitSensor
 from datetime import date
 
+import os, sys
+
+# Get the parent directory
+parent_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Add the parent directory to sys.path
+sys.path.append(parent_dir)
+
 
 def test_monday_open():
     visit_sensor = VisitSensor(1200, 300)
