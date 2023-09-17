d3f97cff062c5a4de87cd1863bbc5d55f082a178
Gautherot <mo.gautherot@laposte.net>
2023-09-17 09:03:33 +0200

    add status code

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fake_data_app import create_app
-from fastapi import FastAPI, Response
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from datetime import date
 
 store_dict = create_app()
@@ -10,25 +11,30 @@ def visit(store_name: str, year: int, month: int, day: int, sensor_id: int | Non
 
     # If the store is the dictionnary
     if not(store_name in store_dict.keys()):
-        return 'Store Not found'
+        return JSONResponse(status_code=404, content='Store Not found')
+    
     
     # Check the value of sensor_id
     if sensor_id and (sensor_id > 7 or sensor_id < 0):
-        return 'Sensor_id Not Found. It must be between 0 and 7'
+        return JSONResponse(status_code=404, content='Sensor_id Not Found. It must be between 0 and 7')
+    
     
     # Check the year
     if year < 2020:
-        return 'No data before 2020'
+        return JSONResponse(status_code=404, content='No data before 2020')
+    
     
     # Check the date
     try: 
         date(year, month, day)
     except:
-        return 'Enter a valid date'
+        return JSONResponse(status_code=404, content='Enter a valid date')
+    
     
     # Check the date is in the past
     if date.today() < date(year, month, day):
-        return 'Choose a date in the past'
+        return JSONResponse(status_code=404, content='Choose a date in the past')
+    
 
     # If no sensor choose return the visit for the whole store
     if sensor_id is None:
@@ -38,8 +44,9 @@ def visit(store_name: str, year: int, month: int, day: int, sensor_id: int | Non
                                                           date(year, month, day))
 
     if visit < 0:
-        return 'The store was closed try another date'
+        return JSONResponse(status_code=404, content='The store was closed try another date')
+    
         
-    return visit
+    return JSONResponse(status_code=200, content=visit)
 
 
